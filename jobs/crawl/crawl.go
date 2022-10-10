5cbab6e9faa9450440c094f68e80412e3464ec8e
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 20:45:36 +0300

    add crawl cron job

---
+++ jobs/crawl/crawl.go
@@ -0,0 +1,29 @@
+// interval job to crawl the web for new content
+package jobs
+
+import (
+	"ip-crawler/db"
+	"ip-crawler/http"
+	"ip-crawler/ip"
+	"ip-crawler/models"
+	"time"
+
+	"github.com/robfig/cron"
+)
+
+func crawl() {
+	address := ip.GetRandom()
+	var inDb models.Ip
+	db.Client.First(&inDb, "address = ?", address)
+	if inDb.Address != "" {
+		return
+	}
+	status := http.Request(address)
+	db.Client.Create(&models.Ip{Address: address, Status: status, CheckedAt: time.Now()})
+}
+
+func Run() {
+	c := cron.New()
+	c.AddFunc("@every 1s", crawl)
+	c.Run()
+}
