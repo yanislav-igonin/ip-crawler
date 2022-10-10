5a306720163b2cd6a17d5335827acb959c88b6b9
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 22:53:35 +0300

    silent logger in gorm

--- db/client.go
+++ db/client.go
@@ -7,12 +7,15 @@ import (
 	// "gorm.io/driver/sqlite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var Client *gorm.DB
 
 func Init() {
-	db, err := gorm.Open(postgres.Open(config.Db.Url), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(config.Db.Url), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	// db, err := gorm.Open(sqlite.Open("local.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
