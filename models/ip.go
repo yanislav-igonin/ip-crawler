9008efd323cbb8d565935399215862851cd691ce
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 22:49:45 +0300

    add index for status field in ip model

--- models/ip.go
+++ models/ip.go
@@ -5,5 +5,5 @@ import "time"
 type Ip struct {
 	Address   string    `gorm:"primaryKey"`
 	CheckedAt time.Time `gorm:"column:checkedAt" sql:"DEFAULT:'current_timestamp'"`
-	Status    string
+	Status    string    `gorm:"index"`
 }
