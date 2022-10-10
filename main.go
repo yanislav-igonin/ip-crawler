efd4224f4425d12b62948b5355ecb88d94089985
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 20:45:46 +0300

    rewrite main func

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"ip-crawler/http"
-	"ip-crawler/ip"
-	"ip-crawler/models"
-
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	Db "ip-crawler/db"
+	Crawl "ip-crawler/jobs/crawl"
 )
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("local.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-	db.AutoMigrate(&models.Ip{})
-
-	address := ip.GetRandom()
-	var inDb models.Ip
-	db.First(&inDb, "address = ?", address)
-	if inDb.Address != "" {
-		return
-	}
-	status := http.Request(address)
-	db.Create(&models.Ip{Address: address, Status: status})
+	Db.Init()
+	Db.Migrate()
+	Crawl.Run()
 }
