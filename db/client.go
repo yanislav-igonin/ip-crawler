978a5dbc0f57edbf375dcd0c5eb182938eaabb1c
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 22:10:14 +0300

    swap from sqlite to postgres

--- db/client.go
+++ db/client.go
@@ -1,16 +1,19 @@
 package db
 
 import (
+	"ip-crawler/config"
 	"ip-crawler/models"
 
-	"gorm.io/driver/sqlite"
+	// "gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var Client *gorm.DB
 
 func Init() {
-	db, err := gorm.Open(sqlite.Open("local.db"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(config.Db.Url), &gorm.Config{})
+	// db, err := gorm.Open(sqlite.Open("local.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
