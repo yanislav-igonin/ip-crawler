db379d7ca131674e02b53fce92f0a739aa35e5e2
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 19:47:35 +0300

    add ranomization for ip address generation

--- ip/generate.go
+++ ip/generate.go
@@ -3,8 +3,10 @@ package ip
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func GetRandom() string {
+	rand.Seed(time.Now().UnixNano())
 	return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256))
 }
