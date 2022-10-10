db11b6aa6170f87d2c3cfd63be36febfa83b3002
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 14:04:41 +0300

    clean

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"ip-crawler/models"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,24 +18,5 @@ func main() {
 	if err != nil {
 		panic("failed to connect database")
 	}
-
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
-
-	// Read
-	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
-	db.First(&product, "code = ?", "D42") // find product with code D42
-
-	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-	// Delete - delete product
-	db.Delete(&product, 1)
+	db.AutoMigrate(&models.Ip{})
 }
