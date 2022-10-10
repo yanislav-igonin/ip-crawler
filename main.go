334efcaef706bceeb3257cc4866f2a4b23ee3080
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 22:10:01 +0300

    rename imports

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	_ "github.com/joho/godotenv/autoload"
 
-	Db "ip-crawler/db"
-	Crawl "ip-crawler/jobs/crawl"
+	"ip-crawler/config"
+	"ip-crawler/db"
+	crawl "ip-crawler/jobs/crawl"
 )
 
 func main() {
-	Db.Init()
-	Db.Migrate()
-	Crawl.Run()
+	config.Init()
+	db.Init()
+	db.Migrate()
+	crawl.Run()
 }
