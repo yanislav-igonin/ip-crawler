5c292f680bad623a159fd273803af1e439250ed7
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-10 16:12:03 +0300

    add ips seeding

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"ip-crawler/ip"
 	"ip-crawler/models"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
-
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
 
 func main() {
 	db, err := gorm.Open(sqlite.Open("local.db"), &gorm.Config{})
@@ -19,4 +15,21 @@ func main() {
 		panic("failed to connect database")
 	}
 	db.AutoMigrate(&models.Ip{})
+
+	fmt.Println("Generating IPs...")
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			for k := 0; k < 256; k++ {
+				var ips []models.Ip
+
+				for l := 0; l < 256; l++ {
+					address := ip.Generate(i, j, k, l)
+					ips = append(ips, models.Ip{Address: address})
+				}
+				db.Create(&ips)
+			}
+			fmt.Printf("Range %d.%d.x.x saved!\n", i, j)
+		}
+		fmt.Printf("Range %d.x.x.x saved!\n", i)
+	}
 }
